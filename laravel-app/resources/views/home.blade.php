1a49bbe07ab62448d73b3f0d4658d71d8197b788
dhaneesh <dhaneesh88@gmail.com>
2025-08-01 10:04:00 +0000

    all files.

---
+++ laravel-app/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home - Laravel Codespace</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="antialiased text-center">
+    <h1>Welcome to Laravel in GitHub Codespaces 🎉</h1>
+    <p>This is your Home page served from <code>HomeController</code>.</p>
+</body>
+</html>
